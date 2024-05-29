acfae4ac8549b35a4c4eb6fed3f71ee887ba1b2c
Tec Tales <dayuking0923@gmail.com>
2024-05-29 23:05:14 +0530

    complete post page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,8 @@ import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
+import PostPage from "./pages/PostPage";
+
 
 export default function App() {
   return (
@@ -28,6 +30,7 @@ export default function App() {
           <Route path="/create-post" element={<CreatePost />} />
           <Route path="/update-post/:postId" element={<UpdatePost />} />
         </Route>
+        <Route path="/post/:postSlug" element={<PostPage />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<Signup />} />
         <Route path="/projects" element={<Projects />} />
